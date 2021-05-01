44b1fbe872dbe6387b903b0ae80ea4f4177bb5bc
jonah0502 <jonah0502@gmail.com>
2021-05-01 14:46:41 -0700

    finished questions 1-3

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ def calcVolume(side):
     return abs(side**3)
 def Average(lst):
     return sum(lst) / len(lst)
+def fullName(first, last):
+    return first + " " + last
--- test_main.py
+++ test_main.py
@@ -3,6 +3,8 @@ import main as code
 import math
 
 class TestCase(unittest.TestCase):
+
+
     def test_volume_1(self):
         self.assertEqual(code.calcVolume(-3), 27)
     @unittest.expectedFailure
@@ -11,5 +13,36 @@ class TestCase(unittest.TestCase):
     @unittest.expectedFailure
     def test_volume_3(self):
         self.assertEqual(code.calcVolume(math.sqrt(-1)), 1)
+
+
+
+
+
+    @unittest.expectedFailure
+    def test_average_1(self):
+        self.assertEqual(code.Average([]), 0)
+        
+    @unittest.expectedFailure
+    def test_average_2(self):
+        self.assertEqual(code.Average(["a", "b", "c"]), TypeError())
+    
+    @unittest.expectedFailure
+    def test_average_3(self):
+        self.assertEqual(code.Average(["1", "2", "3"]), TypeError())
+
+
+
+
+
+    @unittest.expectedFailure
+    def test_name_1(self):
+        self.assertEqual(code.fullName(1, 2), TypeError())
+
+    def test_name_2(self):
+        self.assertEqual(code.fullName("1", "2"), "1 2")
+
+    def test_name_3(self):
+        self.assertEqual(code.fullName(" ", " "), "   ")
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
